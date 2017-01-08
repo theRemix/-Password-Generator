adf4e64db5367c6d415ad517e52838b624633d10
maliaoreta <maliaoreta@gmail.com>
2017-01-07 17:22:11 -1000

    Generates random password for user

--- main.go
+++ main.go
@@ -1,22 +1,46 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"./routes"
+)
+
+const (
+	staticDir   = "./public"
+	defaultPort = "8000"
+)
 
 func main() {
-	dir := "./build"
-
 	r := mux.NewRouter()
-	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir(dir))))
-	r.HandleFunc("/api", ApiHandler)
+	r.HandleFunc("/api/test", routes.TestApiHandler)
+	r.HandleFunc("/api/check", routes.CheckApiHandler)
+	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir(staticDir))))
 	http.Handle("/", r)
 
+	host := "0.0.0.0"
+	port := getEnvOrDefault("PORT", defaultPort)
+	listenAddr := host + ":" + port
+
+	log.Printf("Server listening on %s", listenAddr)
 	srv := &http.Server{
 		Handler: r,
-		Addr:    "127.0.0.1:8000",
+		Addr:    listenAddr,
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
 	log.Fatal(srv.ListenAndServe())
+}
+
+func getEnvOrDefault(env string, fallback string) string {
+	if os.Getenv(env) != "" {
+		return os.Getenv(env)
+	}
+	return fallback
 }
--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,19 +1,57 @@
 import Inferno from 'inferno';
+import Component from 'inferno-component';
 
-export function FormComponent() {
-	return (
-		<fieldset>
-      <div className="field">
-        <label htmlFor="username">Enter Your Username</label>
-        <input id="username" type="text" name="username" />
+export class FormComponent extends Component{
+
+ constructor() {
+    super();
+    this.state = {
+      username: null,
+      errors: null
+    };
+    this.updateUsername = this.updateUsername.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  updateUsername(event) {
+    this.setState({username: event.target.value});
+  }
+
+  submit(event) {
+    event.preventDefault();
+    // handle form submission
+    const req = new Request("/api/check", { method: "POST", body: `{"username": "${this.state.username}", "timestamp": "${Date.now().toString()}"}` });
+    fetch(req)
+      .then(res => res.json())
+      .then(res => {
+        let result = res.password ?
+          `Your new password is: ${res.password}` :
+          "Sorry! You don't get a password";
+        this.setState({ result });
+      })
+      .catch(err => {
+        let errors = err instanceof SyntaxError ?
+          "Server found a broken" :
+          err.toString();
+        this.setState({ errors });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.result}</h2>
+        <form action="#" onSubmit={this.submit}>
+          <div className="field">
+            <label htmlFor="username">Enter Your Username</label>
+            <input id="username" type="text" name="username" onChange={this.updateUsername} />
+          </div>
+          <div className="field">
+            <button type="submit">Get Password</button>
+          </div>
+        </form>
+        <div className="error">{this.state.errors}</div>
       </div>
-      <div className="field">
-        <label htmlFor="password">Enter Your Password</label>
-        <input id="password" type="text" name="password" />
-      </div>
-      <div className="field">
-        <button type="button">Check</button>
-      </div>
-		</fieldset>
-	);
+    );
+  }
 }
